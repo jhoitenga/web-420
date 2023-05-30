9022a4c3fb42daff82c6bb843215dbbf607cf68a
jhoitenga <116404784+jhoitenga@users.noreply.github.com>
2023-05-30 07:12:32 -0500

    adding comments

--- app.js
+++ app.js
@@ -1,23 +1,33 @@
-// Require statements for express, http, swagger-ui-express, swagger-jsdoc, and mongoose
+/*
+Title: app.js
+Author: Professor Krasso
+Date: May 30, 2023
+Modified By: Jennifer Hoitenga
+Description: Creating web-420 GitHub repository
+Sources Used: 
+WEB 420 RESTful APIs assignment instructions
+*/
+
+// Require statements for express, http, swagger-ui-express, swagger-jsdoc, and mongoose.
 const express = require('express');
 const http = require('http');
 const swaggerUi = require('swagger-ui-express');
 const swaggerJsdoc = require('swagger-jsdoc');
 const mongoose = require('mongoose');
 
-// Create a new variable named app and assign it to express library 
+// Creating a new variable named app and assign it to express library. 
 const app = express();
 
-// Set the port to 3000
+// Setting the port to 3000.
 const PORT = process.env.PORT || 3000;
 
-// Set the app to use express.json()
+// Setting the app to use express.json().
 app.use(express.json());
 
-// Set the app to use url encoded
+// Setting the app to use url encoded.
 app.use(express.urlencoded({ extended: true }));
 
-// Object literal
+// Defining an object literal with named options.
 const options = {
     definition: {
         openapi: '3.0.0',
@@ -26,16 +36,16 @@ const options = {
             version: '1.0.0',
         },
     },
-    apis: ['./routes/*.js'], //files containing annotations for the OpenAPI specification
+    apis: ['./routes/*.js'], // Files containing annotations for the OpenAPI specification.
 };
 
-// Creating a new variable name openapiSpecification & calling the swaggerJsdoc library using the options object literal
+// Creating a new variable name openapiSpecification & calling the swaggerJsdoc library using the options object literal.
 const openapiSpecification = swaggerJsdoc(options);
 
-// Wire openapiSpecification variable to app variable
+// Wiring openapiSpecification variable to app variable.
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openapiSpecification));
 
-// Listening
+// Start the server and make it listen on port 3000.
 app.listen(PORT, () => {
     console.log(`Application started and listening on port ${PORT}`);
   });
